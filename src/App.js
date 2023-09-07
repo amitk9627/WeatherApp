0c149d1f2e2cf5f86c330e1cb2ce8dc3274c78cd
Amit kumar <120603787+amitk9627@users.noreply.github.com>
2023-09-07 13:49:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 function App() {
   const [data,setDate]=useState({});
-  const [location,setLocation]=useState('')
+  const [location,setLocation]=useState('mumbai');
 
   const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e2762bec35846609a015787a6526caa8`;
    const searchLocation=(event)=>{
